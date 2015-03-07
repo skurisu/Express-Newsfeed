1f2feeb908016a6fd87e7c085807dacfced1c4f3
Jesse Copeland <jesse.copeland@gmail.com>
2015-03-06 20:10:25 -1000

    added bootstrap, styled login page

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    gulp.src('app/sass/**/*.scss')
+    gulp.src('server/sass/styles.scss')
     .pipe(sass())
-    .pipe(gulp.dest('app/public/css'));
+    .pipe(gulp.dest('server/public/css'));
+
+    gulp.src('server/sass/admin_styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('server/public/css'));
 });
 
+
+
 gulp.task('watch', function () {
-  gulp.watch('app/sass/**/*.scss', ['sass']);
+  gulp.watch('server/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass','watch']);
